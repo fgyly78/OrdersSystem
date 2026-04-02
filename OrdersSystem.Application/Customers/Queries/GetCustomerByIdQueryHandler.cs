89c97e8c04cdd341af0f1d519ac357d1936c2cc2
Александр Ларионов <fgyly78@gmail.com>
2026-04-02 16:48:22 +0300

    feat: completed customers

---
+++ OrdersSystem.Application/Customers/Queries/GetCustomerByIdQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using OrdersSystem.Application.Common.Interfaces;
+using OrdersSystem.Domain.Common;
+using OrdersSystem.Domain.Entities;
+using OrdersSystem.Domain.Repositories;
+using OrdersSystem.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdersSystem.Application.Customers.Queries
+{
+    public class GetCustomerByIdQueryHandler : IRequestHandler<GetCustomerByIdQuery, CustomerDto>
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetCustomerByIdQueryHandler(ICustomerRepository customerRepository, IUnitOfWork unitOfWork)
+        {
+            _customerRepository = customerRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<CustomerDto> Handle(GetCustomerByIdQuery query, CancellationToken ct)
+        {
+            var customer = await _customerRepository.GetByIdAsync(new CustomerId(query.Id), ct);
+
+            if (customer is null) throw new DomainException("Customer not found");
+
+            return new CustomerDto
+            {
+                Id = customer.Id.Value,
+                Email = customer.Email.Value,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Address = $"{customer.Address.Street}, {customer.Address.City}, {customer.Address.Country}, {customer.Address.PostalCode}",
+                CreatedAt = customer.CreatedAt,
+                IsActive = customer.IsActive
+            };
+        }
+    }
+}
